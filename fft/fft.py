9e63d11ec038d62427b13833c3150dde28d4f3d9
Mikołaj Kamiński <mkaminski321+github@gmail.com>
2017-11-22 18:27:51 +0100

    FFT example

--- fft/fft.py
+++ fft/fft.py
@@ -9,7 +9,7 @@ freq3 = 70    # thrid signal frequency in Hz
 t = np.linspace(0, 5, N)    # time vector
 signal = np.sin((freq1 * 2 * np.pi) * t) + np.sin((freq2 * 2 * np.pi) * t) + np.sin((freq3 * 2 * np.pi) * t)      #signal vector
 
-signal = ([1] * 8 + [0] * 8) * 64
+#signal = ([1] * 8 + [0] * 8) * 64      # rectangular pulse
 
 sample_rate = t.__len__()/t[-1]     # calculated sample rate
 
